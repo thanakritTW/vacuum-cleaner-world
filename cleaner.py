e85a065885400922f59b0d9201ca6e1ab25e9980
Gwan Thanakrit <thanakrit.j@thoughtworks.com>
2019-11-28 18:33:36 +0700

    Add core functions

--- cleaner.py
+++ cleaner.py
@@ -1,9 +1,41 @@
-
+import math
 
 class Cleaner:
-
+    _actions = ["clean", "turn_left", "turn_right", "go_forward", "turn_off"]
+    
     def __init__(self):
-        pass
+        self._position = (0,0)
+        self._direction = 0
 
-    def getActions(self):
-        return []
+    def act(self, action):
+        if action not in self._actions:
+            raise Exception("Invalid move")
+        
+        if action == "go_forward":
+            self._go_forward()
+            
+        if action == "turn_left":
+            self._turn_left()
+
+        if action == "turn_right":
+            self._turn_right()
+
+        return True
+
+    def _go_forward(self):
+        x, y = self._position[0], self._position[1] 
+        rad = math.radians(self._direction)
+        self._position = (int(x + math.cos(rad)), int(y + math.sin(rad)))
+
+    def _turn_right(self):
+        self._direction = self._direction + 90
+        if (self._direction >= 360):
+            self._direction -= 360
+    
+    def _turn_left(self):
+        self._direction = self._direction - 90
+        if (self._direction < 0):
+            self._direction += 360
+
+
+        
--- cleaner_test.py
+++ cleaner_test.py
@@ -9,6 +9,79 @@ class TestCleaner(unittest.TestCase):
         self.cleaner = cleaner.Cleaner()
 
     def test_cleaner_should_have_five_actions(self):
-        actions = self.cleaner.getActions()
+        actions = self.cleaner._actions
 
         self.assertIn("clean", actions)
+        self.assertIn("turn_off", actions)
+        self.assertIn("go_forward", actions)
+        self.assertIn("turn_left", actions)
+        self.assertIn("turn_right", actions)
+
+    def test_should_raise_if_invalid_action(self):
+        with self.assertRaises(Exception):
+            self.cleaner.act("invalid_action")
+            
+    def test_should_return_true_if_action_is_valid(self):
+        valid_action = "valid_action"
+        self.cleaner._actions.append(valid_action)
+
+        result = self.cleaner.act(valid_action)
+
+        self.assertTrue(result)
+
+    def test_should_start_with_position_0_0(self):
+        self.assertEqual(self.cleaner._position, (0,0))
+
+    def test_should_start_with_direction_positive_x(self):
+        self.assertEqual(self.cleaner._direction, 0)
+
+    def test_should_move_forward_when_go_forward(self):
+        action = "go_forward"
+
+        self.cleaner._direction = 0
+        self.cleaner.act(action)
+        self.assertEqual(self.cleaner._position, (1,0))
+
+        self.cleaner._direction = 90
+        self.cleaner.act(action)
+        self.assertEqual(self.cleaner._position, (1,1))
+
+        self.cleaner._direction = 180
+        self.cleaner.act(action)
+        self.assertEqual(self.cleaner._position, (0,1))
+
+        self.cleaner._direction = 270
+        self.cleaner.act(action)
+        self.assertEqual(self.cleaner._position, (0,0))
+
+    def test_should_turn_right_when_turn_right(self):
+        action = "turn_right"
+
+        self.cleaner._direction = 0
+        self.cleaner.act(action)
+        self.assertEqual(self.cleaner._direction, 90)
+
+        self.cleaner.act(action)
+        self.assertEqual(self.cleaner._direction, 180)
+
+        self.cleaner.act(action)
+        self.assertEqual(self.cleaner._direction, 270)
+        
+        self.cleaner.act(action)
+        self.assertEqual(self.cleaner._direction, 0)
+        
+    def test_should_turn_left_when_turn_left(self):
+        action = "turn_left"
+
+        self.cleaner._direction = 0
+        self.cleaner.act(action)
+        self.assertEqual(self.cleaner._direction, 270)
+
+        self.cleaner.act(action)
+        self.assertEqual(self.cleaner._direction, 180)
+
+        self.cleaner.act(action)
+        self.assertEqual(self.cleaner._direction, 90)
+        
+        self.cleaner.act(action)
+        self.assertEqual(self.cleaner._direction, 0)
