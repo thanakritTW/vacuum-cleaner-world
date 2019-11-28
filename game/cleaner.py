f88bfa9d64ca18e96c7d199ff6aeaed5fee71413
Thanakrit Juthamongkhon <thanakrit.j@thoughtworks.com>
2019-11-29 00:01:21 +0700

    Modify cleaner to return new object after performing an action

--- game/cleaner.py
+++ game/cleaner.py
@@ -12,15 +12,15 @@ class Cleaner:
             raise Exception("Invalid move")
 
         if action == "go_forward":
-            self._go_forward()
+            return self._go_forward()
 
         if action == "turn_left":
-            self._turn_left()
+            return self._turn_left()
 
         if action == "turn_right":
-            self._turn_right()
+            return self._turn_right()
 
-        return True
+        return self
 
     def position(self):
         return self._position
@@ -32,18 +32,18 @@ class Cleaner:
         x, y = self._position[0], self._position[1]
         rad = math.radians(self._direction)
         new_position = (int(x + math.cos(rad)), int(y + math.sin(rad)))
-        self._position = new_position
+        return Cleaner(new_position, self._direction)
 
     def _turn_right(self):
         new_direction = self._direction + 90
         if (new_direction >= 360):
             new_direction -= 360
-        self._direction = new_direction
+        return Cleaner(self._position, new_direction)
 
     def _turn_left(self):
         new_direction = self._direction - 90
         if (new_direction < 0):
             new_direction += 360
-        self._direction = new_direction
+        return Cleaner(self._position, new_direction)
 
 
--- game/cleaner_test.py
+++ game/cleaner_test.py
@@ -26,13 +26,13 @@ class CleanerTestCase(unittest.TestCase):
         with self.assertRaises(Exception):
             self.cleaner.act("invalid_action")
 
-    def test_should_return_true_if_action_is_valid(self):
+    def test_should_return_new_instance_if_action_is_valid(self):
         valid_action = "valid_action"
         self.cleaner._actions.append(valid_action)
 
-        result = self.cleaner.act(valid_action)
+        cleaner = self.cleaner.act(valid_action)
 
-        self.assertTrue(result)
+        self.assertIsInstance(cleaner, Cleaner)
 
     def test_should_start_with_position_0_0(self):
         self.assertEqual(self.cleaner.position(), (0,0))
@@ -44,49 +44,55 @@ class CleanerTestCase(unittest.TestCase):
         action = "go_forward"
 
         self.cleaner._direction = 0
-        self.cleaner.act(action)
-        self.assertEqual(self.cleaner.position(), (1,0))
+        cleaner = self.cleaner.act(action)
+        self.assertEqual(cleaner.position(), (1,0))
 
         self.cleaner._direction = 90
-        self.cleaner.act(action)
-        self.assertEqual(self.cleaner.position(), (1,1))
+        cleaner = self.cleaner.act(action)
+        self.assertEqual(cleaner.position(), (0,1))
 
         self.cleaner._direction = 180
-        self.cleaner.act(action)
-        self.assertEqual(self.cleaner.position(), (0,1))
+        cleaner = self.cleaner.act(action)
+        self.assertEqual(cleaner.position(), (-1,0))
 
         self.cleaner._direction = 270
-        self.cleaner.act(action)
-        self.assertEqual(self.cleaner.position(), (0,0))
+        cleaner = self.cleaner.act(action)
+        self.assertEqual(cleaner.position(), (0,-1))
 
     def test_should_turn_right_when_turn_right(self):
         action = "turn_right"
 
         self.cleaner._direction = 0
-        self.cleaner.act(action)
-        self.assertEqual(self.cleaner.direction(), 90)
+        cleaner = self.cleaner.act(action)
+        self.assertEqual(cleaner.direction(), 90)
 
-        self.cleaner.act(action)
-        self.assertEqual(self.cleaner.direction(), 180)
+        self.cleaner._direction = 90
+        cleaner = self.cleaner.act(action)
+        self.assertEqual(cleaner.direction(), 180)
 
-        self.cleaner.act(action)
-        self.assertEqual(self.cleaner.direction(), 270)
+        self.cleaner._direction = 180
+        cleaner = self.cleaner.act(action)
+        self.assertEqual(cleaner.direction(), 270)
 
-        self.cleaner.act(action)
-        self.assertEqual(self.cleaner.direction(), 0)
+        self.cleaner._direction = 270
+        cleaner = self.cleaner.act(action)
+        self.assertEqual(cleaner.direction(), 0)
 
     def test_should_turn_left_when_turn_left(self):
         action = "turn_left"
 
         self.cleaner._direction = 0
-        self.cleaner.act(action)
-        self.assertEqual(self.cleaner.direction(), 270)
+        cleaner = self.cleaner.act(action)
+        self.assertEqual(cleaner.direction(), 270)
 
-        self.cleaner.act(action)
-        self.assertEqual(self.cleaner.direction(), 180)
+        self.cleaner._direction = 270
+        cleaner = self.cleaner.act(action)
+        self.assertEqual(cleaner.direction(), 180)
 
-        self.cleaner.act(action)
-        self.assertEqual(self.cleaner.direction(), 90)
+        self.cleaner._direction = 180
+        cleaner = self.cleaner.act(action)
+        self.assertEqual(cleaner.direction(), 90)
 
-        self.cleaner.act(action)
-        self.assertEqual(self.cleaner.direction(), 0)
+        self.cleaner._direction = 90
+        cleaner = self.cleaner.act(action)
+        self.assertEqual(cleaner.direction(), 0)
